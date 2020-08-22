abbc6d09a9b7e0777d416f9b26e14a332d9f9b88
camshaft54 <cam.shaw1234@gmail.com>
2020-08-22 10:05:02 -0700

    Fix Maven errors Add config Add payment system

--- src/main/java/io/github/camshaft54/jsonbooks/JSONBooks.java
+++ src/main/java/io/github/camshaft54/jsonbooks/JSONBooks.java
@@ -2,12 +2,29 @@ package io.github.camshaft54.jsonbooks;
 
 import io.github.camshaft54.jsonbooks.commands.JSONBooksCommands;
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class JSONBooks extends JavaPlugin {
 
+    FileConfiguration config = this.getConfig();
+    public static Material paymentItem;
+    public static int paymentAmount;
+    public static String paymentItemString;
+    public static Boolean cmdAllowed;
+
     @Override
     public void onEnable() {
+        config.addDefault("payment", "diamond");
+        config.addDefault("amount", 2);
+        config.addDefault("cmdAllowed",true);
+        config.options().copyDefaults(true);
+        saveConfig();
+        paymentItem = Material.valueOf(config.getString("payment").toUpperCase());
+        paymentAmount = config.getInt("amount");
+        paymentItemString = config.getString("payment");
+        cmdAllowed = config.getBoolean("cmdAllowed");
         JSONBooksCommands commands = new JSONBooksCommands();
         getCommand("jsonbook").setExecutor(commands);
         getServer().getConsoleSender().sendMessage(ChatColor.DARK_BLUE + "[JSONBooks]: Plugin is enabled!");
--- src/main/java/io/github/camshaft54/jsonbooks/commands/JSONBooksCommands.java
+++ src/main/java/io/github/camshaft54/jsonbooks/commands/JSONBooksCommands.java
@@ -1,6 +1,8 @@
 package io.github.camshaft54.jsonbooks.commands;
 
+import io.github.camshaft54.jsonbooks.JSONBooks;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -9,40 +11,64 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
 public class JSONBooksCommands implements CommandExecutor {
 
     @Override
-    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+    public boolean onCommand(CommandSender commandSender, Command command, String label, String[] args) {
         if (!(commandSender instanceof Player))
             return true;
         Player player = (Player) commandSender;
-
-        if (command.getName().equalsIgnoreCase("jsonbook")) {
-            if (strings.length >= 1) {
-                String json = "";
-                try {
-                    Document doc = null;
-                    player.sendMessage("JSONBooks: " + strings[0]);
-                    doc = Jsoup.connect(strings[0]).get();
-                    json = doc.body().text();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                player.sendMessage(json);
-                ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
-                Bukkit.getUnsafe().modifyItemStack(book, json.toString());
-                player.getInventory().addItem(book);
-            }
-            else {
-                player.sendMessage("JSONBooks: Invalid Arguments\nUsage: /jsonbook <json>");
-            }
+        if (args.length != 1) {
+            player.sendMessage("JSONBooks: Invalid Arguments\nUsage: /jsonbook <json>");
+            return true;
         }
 
+        ItemStack payment = new ItemStack(JSONBooks.paymentItem);
+        int paymentAmount = JSONBooks.paymentAmount;
+        ItemStack fullPayment = new ItemStack(JSONBooks.paymentItem, paymentAmount);
+        // Get JSON and check if it contains runCommand
+        String json = getJSON(player, args[0],JSONBooks.cmdAllowed);
+        if (json == null) {
+            return true;
+        }
+        // If the player is in creative mode they do not need to pay
+        if (player.getGameMode() == GameMode.CREATIVE || paymentAmount <= 0) {
+            giveBook(player, json);
+            return true;
+        }
+        // Check player's inventory for the payment
+        if (player.getInventory().containsAtLeast(payment, paymentAmount)) {
+            player.sendMessage(player.getDisplayName() + " paid " + paymentAmount + " x " + JSONBooks.paymentItemString + " for 1 book.");
+            giveBook(player, json);
+            player.getInventory().removeItem(fullPayment);
+            return true;
+        }
+        player.sendMessage("Insufficient funds. The set payment for this command is " + JSONBooks.paymentAmount + " x " + JSONBooks.paymentItemString + ".");
         return true;
+    }
+
+    private String getJSON(Player player, String link, Boolean cmdAllowed) {
+        String json = "";
+        try {
+            Document doc = Jsoup.connect(link).get();
+            json = doc.body().text();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if (!cmdAllowed && json.contains("\"action\":\"run_command\"") && player.getGameMode() != GameMode.CREATIVE) {
+            player.sendMessage("JSONBooks: Running commands in books has been disallowed by the server administrator.");
+            return null;
+        }
+        return json;
+    }
+
+    private void giveBook(Player player, String json) {
+        ItemStack book = new ItemStack(Material.WRITTEN_BOOK);
+        Bukkit.getUnsafe().modifyItemStack(book, json);
+        player.sendMessage("JSONBooks: Gave " + player.getDisplayName() + " 1 JSON Book");
+        player.getInventory().addItem(book);
     }
 }
